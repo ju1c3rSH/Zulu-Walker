7a465eae51bfa590fbc48635d2e3305f04393697
LongEP <2115513882@qq.com>
2026-05-18 14:06:20 +0800

    refactor: - 修改了靶子内参

--- utils/focal_distance_util.py
+++ utils/focal_distance_util.py
@@ -13,7 +13,7 @@ from typing import Tuple
 
 # 目标实际尺寸 (mm)
 reference_size_dict = {
-    "quad": (290.0, 200.0, 245.0),  # mm (宽, 高, 平均)
+    "quad": (264.0, 177.0, 220.5),  # mm (宽, 高, 平均)
 }
 
 @dataclass
