6b8188c1d531e6f47e917a8a7ccda3289df4169f
LongEP <2115513882@qq.com>
2026-07-04 00:01:47 +0800

    feat: 在 detectors/__init__.py 中导出 CargoDetector 和 CargoDetectMethod

--- modules/zw_opencv_module/detectors/__init__.py
+++ modules/zw_opencv_module/detectors/__init__.py
@@ -1,4 +1,9 @@
 from .circle_target_detector import CircleTargetDetector
-from .circle_target_detector.detection import DetectMethod
+from .circle_target_detector.detection import DetectMethod as CircleDetectMethod
+from .cargo_detector import CargoDetector
+from .cargo_detector.detection import DetectMethod as CargoDetectMethod
 
-__all__ = ["CircleTargetDetector", "DetectMethod"]
+__all__ = [
+    "CircleTargetDetector", "CircleDetectMethod",
+    "CargoDetector", "CargoDetectMethod",
+]
