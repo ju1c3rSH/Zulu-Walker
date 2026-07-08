31880d24f40b3aaf5880659efc6c3cec9a9c4843
LongEP <2115513882@qq.com>
2026-07-08 09:17:14 +0800

    detector: add RingDetector + FastRingMethod + EdgeDrawingRingMethod, rewrite RingDiscoveryProcessor to delegate to RingDetector (follows CargoDetector pattern)

---
+++ modules/zw_opencv_module/detectors/ring_detector/__init__.py
@@ -0,0 +1,206 @@
+import cv2
+import numpy as np
+from typing import Optional, Tuple, Dict, Any
+from collections import deque
+
+try:
+    cv2.ximgproc.createEdgeDrawing
+    _HAS_XIMGPROC = True
+except AttributeError:
+    _HAS_XIMGPROC = False
+
+from ...models.color import Color
+from ...models.ring import RingTarget
+from .detection import RingDetectMethod
+
+
+class _TrackingState:
+
+    __slots__ = (
+        "kf", "tracking_initialized", "lost_frames", "_last_predict_time",
+        "last_center", "roi_miss_count", "_center_history",
+    )
+
+    def __init__(self, smooth_window: int = 5):
+        self.kf = cv2.KalmanFilter(4, 2)
+        self.kf.transitionMatrix = np.array(
+            [[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32
+        )
+        self.kf.measurementMatrix = np.array(
+            [[1, 0, 0, 0], [0, 1, 0, 0]], np.float32
+        )
+        self.kf.processNoiseCov = np.eye(4, dtype=np.float32) * 0.1
+        self.kf.measurementNoiseCov = np.eye(2, dtype=np.float32) * 2.0
+        self.kf.errorCovPost = np.eye(4, dtype=np.float32)
+
+        self.tracking_initialized = False
+        self.lost_frames = 0
+        self._last_predict_time = None
+
+        self.last_center: Optional[Tuple[float, float]] = None
+        self.roi_miss_count = 0
+
+        self._center_history: deque = deque(maxlen=smooth_window)
+
+    def resize_histories(self, smooth_window: int):
+        self._center_history = deque(self._center_history, maxlen=smooth_window)
+
+    def reset_prediction(self):
+        self.last_center = None
+        self.roi_miss_count = 0
+        self.tracking_initialized = False
+        self.lost_frames = 0
+        self._last_predict_time = None
+        self._center_history.clear()
+        self.kf.statePost = np.zeros((4, 1), dtype=np.float32)
+        self.kf.errorCovPost = np.eye(4, dtype=np.float32)
+
+
+class RingDetector:
+
+    def __init__(self, name: str = "ring_detect"):
+        self.name = name
+
+        self.ed = None
+        self._init_edge_drawing()
+        self.detect_method = RingDetectMethod.FAST_RING
+
+        self.q_base = 0.2
+        self.q_vel_base = 0.15
+        self.max_lost_frames = 10
+        self.roi_size = 150
+        self.max_roi_miss = 5
+        self.min_area = 100
+        self.smooth_window = 5
+
+        self.blur_kernel = 5
+        self.blur_sigma = 1.5
+        self.ed_min_path_length = 50
+        self.ed_gradient_threshold = 36
+        self.ed_nfa_validation = True
+        self.edge_morph_kernel = 3
+        self.edge_morph_iterations = 1
+
+        self._last_mask = None
+        self._last_morphed = None
+        self._last_edge_preview = None
+
+        self._tracking: Dict[Color, _TrackingState] = {}
+        self._active_tracking: Optional[_TrackingState] = None
+        self._active_color: Optional[Color] = None
+
+        self.color_ranges = {
+            Color.RED: [
+                (np.array([0, 30, 0]), np.array([10, 255, 255])),
+                (np.array([170, 30, 0]), np.array([180, 255, 255])),
+            ],
+            Color.GREEN: [(np.array([40, 50, 50]), np.array([80, 255, 255]))],
+            Color.BLUE: [(np.array([100, 50, 50]), np.array([130, 255, 255]))],
+        }
+
+        self._methods: Dict[RingDetectMethod, Any] = {}
+        self._init_detection_methods()
+
+    def _init_edge_drawing(self):
+        if not _HAS_XIMGPROC:
+            return
+        self.ed = cv2.ximgproc.createEdgeDrawing()
+        self._update_ed_params()
+
+    def _update_ed_params(self):
+        if self.ed is None:
+            return
+        ed_params = self.ed.Params()
+        ed_params.MinPathLength = self.ed_min_path_length
+        ed_params.GradientThresholdValue = self.ed_gradient_threshold
+        ed_params.NFAValidation = self.ed_nfa_validation
+        self.ed.setParams(ed_params)
+
+    def _init_detection_methods(self):
+        from .detection.methods.fast_ring import FastRingMethod
+        from .detection.methods.edge_drawing_ring import EdgeDrawingRingMethod
+        self._methods = {
+            RingDetectMethod.FAST_RING: FastRingMethod(detector=self),
+            RingDetectMethod.EDGE_DRAWING_RING: EdgeDrawingRingMethod(detector=self),
+        }
+
+    def _get_tracking(self, color: Color) -> _TrackingState:
+        if color not in self._tracking:
+            self._tracking[color] = _TrackingState(smooth_window=self.smooth_window)
+        ts = self._tracking[color]
+        self._active_tracking = ts
+        self._active_color = color
+        return ts
+
+    def set_detect_method(self, method: RingDetectMethod) -> bool:
+        if method == self.detect_method:
+            return True
+        if method == RingDetectMethod.EDGE_DRAWING_RING and self.ed is None:
+            return False
+        self.detect_method = method
+        self._reset_all_tracking()
+        return True
+
+    def _reset_all_tracking(self):
+        for ts in self._tracking.values():
+            ts.reset_prediction()
+        self._active_tracking = None
+        self._active_color = None
+
+    def is_edge_drawing_available(self) -> bool:
+        return self.ed is not None
+
+    def get_detect_method(self) -> RingDetectMethod:
+        return self.detect_method
+
+    @staticmethod
+    def get_supported_methods():
+        return [RingDetectMethod.FAST_RING, RingDetectMethod.EDGE_DRAWING_RING]
+
+    def get_method_params(self, method: RingDetectMethod) -> dict:
+        return {
+            "roi_size": self.roi_size,
+            "max_roi_miss": self.max_roi_miss,
+            "min_area": self.min_area,
+            "smooth_window": self.smooth_window,
+            "q_base": self.q_base,
+            "q_vel_base": self.q_vel_base,
+            "max_lost_frames": self.max_lost_frames,
+            "blur_kernel": self.blur_kernel,
+            "blur_sigma": self.blur_sigma,
+            "ed_min_path_length": self.ed_min_path_length,
+            "ed_gradient_threshold": self.ed_gradient_threshold,
+            "ed_nfa_validation": self.ed_nfa_validation,
+            "edge_morph_kernel": self.edge_morph_kernel,
+            "edge_morph_iterations": self.edge_morph_iterations,
+        }
+
+    def update_params(self, params: dict):
+        int_keys = (
+            "roi_size", "max_roi_miss", "min_area",
+            "smooth_window", "blur_kernel", "ed_min_path_length",
+            "ed_gradient_threshold", "edge_morph_kernel", "edge_morph_iterations",
+            "max_lost_frames",
+        )
+        for key in int_keys:
+            if key in params:
+                setattr(self, key, int(params[key]))
+
+        float_keys = ("blur_sigma", "q_base", "q_vel_base")
+        for key in float_keys:
+            if key in params:
+                setattr(self, key, float(params[key]))
+
+        if "ed_nfa_validation" in params:
+            self.ed_nfa_validation = bool(params["ed_nfa_validation"])
+
+        for ts in self._tracking.values():
+            ts.resize_histories(self.smooth_window)
+
+        self._update_ed_params()
+
+    def detect_ring(self, frame, target_color: Color) -> Optional[RingTarget]:
+        method = self._methods.get(self.detect_method)
+        if method is not None:
+            return method.detect(frame, target_color)
+        return None
---
+++ modules/zw_opencv_module/detectors/ring_detector/detection/__init__.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class RingDetectMethod(Enum):
+    FAST_RING = "fast_ring"
+    EDGE_DRAWING_RING = "edge_drawing_ring"
