ddb126ced3a81c01ec3e7534025a43cfdf13e9ed
LongEP <2115513882@qq.com>
2026-08-22 16:54:50 +0800

    feat: add ui_state module with tested button geometry and cooldown gates
    First slice of the P4 god-class split (SCHED-04/D5): pure frame-coordinate hit regions and interaction gates, replacing inline math in main.py's touch callback.

---
+++ app/display/__init__.py
@@ -0,0 +1,5 @@
+"""app.display: this-competition composition & UI orchestration (D5)."""
+
+from .ui_state import CooldownGate, HitRegion, bottom_left_region
+
+__all__ = ["CooldownGate", "HitRegion", "bottom_left_region"]
